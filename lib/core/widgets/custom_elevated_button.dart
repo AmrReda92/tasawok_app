1734e986ef3e68825325ef0eac8c579aefae5ae3
Amr Reda <amr.reda2014@yahoo.com>
2025-12-12 19:18:45 +0200

    finish onboarding screen

---
+++ lib/core/widgets/custom_elevated_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CustomElevatedButton extends StatelessWidget {
+  final void Function() onPressed ;
+  final Widget title ;
+  const CustomElevatedButton({super.key, required this.onPressed, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+         style: ElevatedButton.styleFrom(
+             minimumSize: const Size(65, 58),
+           backgroundColor: Theme.of(context).colorScheme.primary,
+           shape: RoundedRectangleBorder(
+             borderRadius: BorderRadius.circular(12)
+           )
+         ),
+        onPressed: onPressed,
+        child: title
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:practise_three/travel_app.dart';
+
+void main() {
+  runApp(const TravelApp());
+}
+
+
+
