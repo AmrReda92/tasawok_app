fbebb38f2a3fa0496d8296e5f11fa75b00dea5ee
Amr Reda <amr.reda2014@yahoo.com>
2026-01-01 18:35:40 +0200

    create home repo impl

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,12 +1,25 @@
 import 'package:dartz/dartz.dart';
 import 'package:practise_three/core/errors/failure.dart';
+import 'package:practise_three/core/utils/api_service.dart';
 import 'package:practise_three/features/home/data/models/category_model.dart';
 import 'package:practise_three/features/home/data/repos/home_repo.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failure, List<ProductModel>>> getCategoryName() {
+  Future<Either<Failure, List<ProductModel>>> getCategoryName()async {
+   try{
+     final data = await apiService.get(endPoint: "products");
 
+     final List<ProductModel> allProducts = [];
+      for(var item in data["products"]){
+        allProducts.add(ProductModel.fromJson(item));
+      }
+      return right(allProducts);
+   }catch(e){
+    return left(ServerError());
+   }
   }
 
 }
